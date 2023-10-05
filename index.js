6a5e818b78aee574f141977b9d9a22305bf83756
JoltLaw <94019623+JoltLaw@users.noreply.github.com>
2023-10-05 10:31:51 -0600

    script update

--- index.js
+++ index.js
@@ -1,58 +1,49 @@
-const gallery = document.getElementById("gallery");
+const galary = document.getElementById("galary");
 
-function createPiece(piece) {
+const createPiece = (piece) => {
+    const container = document.createElement("div");
+    container.classList = "galaryContainer";
+    const image = document.createElement("img");
+    image.classList = "display";
+    image.src = piece.src;
+    const textContainer = document.createElement("div");
+    const title = document.createElement("h3");
+    title.classList = "artTitle";
+    title.textContent = piece.title;
+    const date = document.createElement("span");
+    date.classList = "createdDate";
+    date.textContent = piece.date;
+    const textBody = document.createElement("p");
+    textBody.classList = "artDis";
+    textBody.textContent = piece.textBody;
 
-  // creating the primary title
-  const container = document.createElement("div");
-  container.classList.add("galleryContainer");
+    // Appending items
+    textContainer.appendChild(title);
+    textContainer.appendChild(date);
+    textContainer.appendChild(textBody);
+    container.appendChild(image);
+    container.appendChild(textContainer);
 
-  // creating the image
-  const image = document.createElement("img");
-  image.classList.add("display");
-  image.src = piece.src;
+    galary.appendChild(container);
 
-  // creating a container for all text elements
-  const textContainer = document.createElement("div");
-
-  // creating the title
-  const title = document.createElement("h3");
-  title.classList.add("artTitle");
-  title.textContent = piece.title;
-
-  // creating the date object
-  const date = document.createElement("span");
-  date.classList.add("createdDate");
-  date.textContent = piece.date;
-
-  // creating the discription
-  const textBody = document.createElement("p");
-  textBody.classList.add("artDis");
-  textBody.textContent = piece.textBody;
-
-  // Appending items
-  textContainer.appendChild(title);
-  textContainer.appendChild(date);
-  textContainer.appendChild(textBody);
-
-  container.appendChild(image);
-  container.appendChild(textContainer);
-
-  gallery.appendChild(container);
 }
 
-const displays = [
-  {
+const displays = [ {
     src: "",
     title: "Title",
     date: "DD/MM/YYYY",
-    textBody: "some sort of description of what the art piece means, why you made it, any stories behind it or any other details you'd want to share about it"
-  },
-  {
-    src: "",
-    title: "Title",
-    date: "DD/MM/YYYY",
-    textBody: "some sort of description of what the art piece means, why you made it, any stories behind it or any other details you'd want to share about it"
-  }
-];
+    textBody: "some sort of discription of what the art piece means, why you made it, any stories behind it or any other details you'd want to share about it"
+},
 
-displays.forEach(item => createPiece(item));
+ {
+    src: "",
+    title: "Title",
+    date: "DD/MM/YYYY",
+    textBody: "some sort of discription of what the art piece means, why you made it, any stories behind it or any other details you'd want to share about it"
+}];
+
+
+
+
+
+displays.forEach(item => createPiece(item))
